2d0d99c08374ee6cb36c115c0c88289708727cad
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2022-03-08 11:33:37 -0500

    winn

--- src/_javaDersAnlatimi/emrah/Pr.java
+++ src/_javaDersAnlatimi/emrah/Pr.java
@@ -1,33 +1,18 @@
 package _javaDersAnlatimi.emrah;
 
 public class Pr {
-
-    public static void main(String[] args) {
-
-        // Fibonacci number is a sequence of whole number where every number is the sum of previous two numbers.
-        // Formula is "Fn = Fn-1 + Fn-2"   ->    0.1.1.2.3.5.8.13........
-
-        // give me first 7 fibonacci numbers  -> 0.1.1.2.3.5.8
-        int range = 7;
-
-        int fibo1 = 0;
-        int fibo2 = 1;
-        int fiboN;
-
-        System.out.print(fibo1+" ");
-        System.out.print(fibo2+" ");
-
-        for(int i=1; i<range-1; i++){
-            fiboN = fibo1 + fibo2;
-            fibo1=fibo2;
-            fibo2=fiboN;
-            System.out.print(fiboN+" ");
-        }
+    /**
+    lines of a half pyramid
+     *
+     *  *
+     *  *  *
+     *  *  *  *
 
 
 
+    */
 
-
+    public static void main(String[] args){
 
 
     }
--- src/_javaDersAnlatimi/suleymanInterview/HalfPyramid.java
+++ src/_javaDersAnlatimi/suleymanInterview/HalfPyramid.java
@@ -11,7 +11,7 @@ package _javaDersAnlatimi.suleymanInterview;
 
 public class HalfPyramid {
     public static void main(String[] args) {
-        int rows = 5;
+        int rows = 4;
 
         for (int i= 1; i <= rows; i++){
             for (int j = 1; j <= i ; j++) {
