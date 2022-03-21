fffb8426798e9130c2e80cb10a4843afa0ed8d6c
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2022-03-20 23:17:44 -0400

    win

--- src/emrah_study/_Day16_PreInterviewAll.java
+++ src/emrah_study/_Day16_PreInterviewAll.java
@@ -1,5 +1,7 @@
 package emrah_study;
 
+import java.util.Scanner;
+
 public class _Day16_PreInterviewAll {
 /**
  1-Reverse String (String tersine cevirme)
@@ -35,6 +37,70 @@ class x01_ReverseString {
         System.out.println("Reverse String: " + rvr);
     }
 }
+
+class x02_ReverseInteger {
+    public static void main(String[] args) {
+        //2-Reverse integer (Integer I tersine cevirme)
+        int num = 123456789;
+        int reversed = 0;
+        System.out.println("Original Number: " + num);
+
+        while (num != 0) {
+            int digit = num % 10;//get last from num with % remainder
+            reversed = reversed * 10 + digit;
+            num /= 10;//remove last digit from num
+        }
+        System.out.println("Reversed Number: " + reversed);
+
+    }
+}
+
+class x03_UniqueCharacters {
+    public static void main(String[] args) {
+        /*
+        Write a return method that can find the unique characters from the String
+        (Dizedeki benzersiz karakterleri bulabilen bir metod yazın)
+        Ex: unique(“AAABBBCCCDEF”) ==> “DEF”;
+         */
+
+        String str = "AAABBBCCCDEF";
+        for (int i = 0; i < str.length(); i++) {
+            int flag = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j) && i != j) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.print(str.charAt(i));
+            }
+        }
+    }
+}
+
+
+class x05_Even$Odd {
+    public static void main(String[] args) {
+        /*
+        Write a method which can identifies given number is even or odd
+        (Verilen sayının çift veya tek olduğunu tanımlayabilecek bir yöntem yazın)
+         */
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Enter a number for checking Even or Odd");
+        int num = sc1.nextInt();
+        System.out.println("Number: "+num);
+
+        if (num % 2 == 0) {
+            System.out.println("Number is EVEN");
+        } else {
+            System.out.println("Number is ODD");
+        }
+        sc1.close();
+    }
+}
+
 
 
 
