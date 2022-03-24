c824390afda1b11b5d8fb487f093686fd778b505
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2022-03-24 09:01:14 -0400

    asda

--- src/emrah_study/pr.java
+++ src/emrah_study/pr.java
@@ -1,46 +1,17 @@
 package emrah_study;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class pr {
     public static void main(String[] args) {
-        //	3-Write a return method that can find the unique characters from the String
-        //		     (Dizedeki benzersiz karakterleri bulabilen bir metod yazın)
-        //		     Ex: unique(“AAABBBCCCDEF”) ==> “DEF”;
-        List<String> list = new ArrayList<>();
+        //1.satir
+        //2.satir
 
-        list.add("A");
-        list.add("A");
-        list.add("A");
-        list.add("B");
-        list.add("B");
-        list.add("B");
-        list.add("C");
-        list.add("C");
-        list.add("C");
-        list.add("D");
-        list.add("E");
-        list.add("F");
 
-        Set<String> hList = new HashSet<>();
-        for (String a: list) {
-            hList.add(a);
-        }
 
-        System.out.println(hList);
-        for (int i=0;i<hList.size();i++) {
 
-            String element = list.get(i);
-            list.remove(element);
-            if (list.contains(element)) {
-                hList.remove(element);
-            }
-        }
-
-        System.out.println(hList);
     }
+
+
+
 }
+
 
