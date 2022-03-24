7aaa62bbfec6b59e148a2bc57e50f4ab0cf17de1
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2022-03-24 09:44:11 -0400

    win

--- src/emrah_study/pr.java
+++ src/emrah_study/pr.java
@@ -2,14 +2,6 @@ package emrah_study;
 
 public class pr {
     public static void main(String[] args) {
-        //1.satir
-        //Ara satir
-        //2.satir
-        //3.satir github
-
-
-
-
 
     }
 
