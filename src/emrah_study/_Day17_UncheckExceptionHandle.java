599b565856ca7706c605b5cbf0532f63d7fb5925
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2022-03-27 09:50:46 -0400

    win

---
+++ src/emrah_study/_Day17_UncheckExceptionHandle.java
@@ -0,0 +1,28 @@
+package emrah_study;
+
+public class _Day17_UncheckExceptionHandle {
+
+    // Main Driver Method
+    public static void main(String[] args) {
+        // Inserting elements into Array
+        int a[] = {1, 2, 3, 4, 5};
+
+        // Try block for exceptions
+        try {
+
+            // Forcefully trying to access and print
+            // element/s beyond indexes of the array
+
+            System.out.println(a[5]);
+        }
+
+        // Catch block for catching exceptions
+        catch (ArrayIndexOutOfBoundsException e) {
+
+            // Printing display message when index not
+            // present in a array is accessed
+            System.out.println(
+                    "Out of index please check your code");
+        }
+    }
+}
